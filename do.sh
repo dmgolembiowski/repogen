f54ecc48efde3165c88c96e6bc1f915b42379590
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2017-12-27 07:51:42 +0530

    refactor

--- do.sh
+++ do.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-
+checksum=`/bin/cat<<++|/bin/md5
 #------------------------------------------------------------------------------
 #
 # Copyright (c) 2017 Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
@@ -17,6 +17,8 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 #------------------------------------------------------------------------------
+++
+`
 
 # Usage:
 # do.sh -	will mirror an OpenBSD CVS mirror, make a copy of that mirror, 
@@ -25,7 +27,10 @@
 #		user should aim it a particular openbsd rsync mirror 
 #		and run it regularly to maintain updated git mirrors
 #
+#
+# $ doas pkg_add git
 # $ doas pkg_add cvs2gitdump
+# $ doas pkg_add rsync
 # $ mkdir repo
 # $ cd repo
 # $ git clone https://github.com/hakrtech/repogen.git
@@ -42,6 +47,7 @@
 # 8.	xenocara0/	- same for xenocara
 # 9.	ports0/		- same for ports
 # 10.	www0/		- same for www
+#
 # and you run it again to update the same
 # $ ./repogen/do.sh
 # will update
@@ -55,6 +61,15 @@
 # 8.	xenocara0/	- update same for xenocara
 # 9.	ports0/		- update same for ports
 # 10.	www0/		- update same for www
+
+licenseok() {
+	if [ $checksum != "32c66371f4be6ca567cd523f7f3c55a0" ]; then
+		echo $0: license tampered, verify with https://cvsweb.openbsd.org/cgi-bin/cvsweb/src/share/misc/license.template?rev=HEAD  
+		exit 1
+	fi
+}
+
+licenseok
 
 rsynchostpath=anoncvs.fr.openbsd.org/openbsd-cvs/
 
@@ -83,6 +98,40 @@ if [ $memcache -eq 1 ]; then
 	fi
 fi
 
+require() {
+	binpath=/usr/local/bin/$binfile
+	if [ ! -x $f ]; then
+		echo "$0: error cannot run $binpath
+		exit 1
+	fi
+	if [ ! -f $binpath ]; then
+		echo "$0: error cannot without $binpath, install it
+		exit 1
+	fi
+}
+
+for binfile in rsync git cvs2gitdump
+do
+	require $binfile
+done
+cvs2gitdump=/usr/local/bin/cvs2gitdump
+
+# memcache needs doas, if unable to run command as root, then no memcache
+if [ $memcache -eq 1 ]; then
+	/usr/bin/doas -n /usr/bin/id
+	if [ $? -ne 0 ]; then
+		memcache=0
+		echo MARK unset memcache as unable to do passwordless doas
+	fi
+fi
+
+mark() {
+	echo -n "MARK "
+	date
+}
+
+mark
+
 physmem=`sysctl -n hw.physmem`
 memlim=10737418240 # 10GB
 # use memory files if we have more than 10GB RAM because cvsrepo0 is about 6.3GB
@@ -93,50 +142,45 @@ fi
 # incoming cvs repository - cvsrepo0
 cvsrepo=`pwd`/cvsrepo0
 if [ $upsync -eq 1 ]; then
-	echo MARK
-	date
-	rsync -avz --delete rsync://$rsynchostpath cvsrepo0
-	date
+	mark
+	/usr/local/bin/rsync -az --delete rsync://$rsynchostpath cvsrepo0
+	mark
 fi
 
 # sync incoming with staging on disk
 stagesync=1	# do not change
 if [ $stagesync -eq 1 ]; then
-	echo MARK
-	rsync -a --delete cvsrepo0/ cvsrepo1
-	date
-	echo MARK
+	mark
+	/usr/local/bin/rsync -a --delete cvsrepo0/ cvsrepo1
+	mark
 	cvsrepo=`pwd`/cvsrepo1
 fi
 
 # sync staging on disk with staging in memory
 if [ $memcache -eq 1 ]; then
-	echo MARK
-	date
+	mark
 	if [ -d /m ]; then
-		doas umount -f /m
-		sleep 10
-		doas rmdir /m
+		/usr/bin/doas /sbin/umount -f /m
+		/bin/sleep 10
+		/usr/bin/doas /bin/rmdir /m
 	fi
-	doas mkdir /m
-	doas mount -t mfs -o rw,noatime,noexec,nodev,nosuid,-s=9g $device /m
-	doas chown $USER /m
-	mount
+	/usr/bin/doas /bin/mkdir /m
+	/usr/bin/doas /sbin/mount -t mfs -o rw,noatime,noexec,nodev,nosuid,-s=9g $device /m
+	/usr/bin/doas /sbin/chown $USER /m
+	/sbin/mount
 	ls -ld /m
 	df -h
-	sleep 10
+	/bin/sleep 10
 
 	if [ ! -d /m/cvsrepo1 ]; then
-		doas chown -R $USER /m
-		mkdir -p /m/cvsrepo1
-		cp -r cvsrepo1/ /m
-		date
-		echo MARK
+		/usr/bin/doas /sbin/chown $USER /m
+		/bin/cp -r cvsrepo1/ /m
+		mark
 	fi
 
-	rsync -a --delete cvsrepo1/ /m/cvsrepo1
-	date
-	echo MARK
+	mark
+	/usr/local/bin/rsync -av --delete cvsrepo1/ /m/cvsrepo1
+	mark
 
 	cvsrepo=/m/cvsrepo1
 fi
@@ -150,81 +194,77 @@ do
 	repodir=bare.${module}.git
 	gitrepo=`pwd`/bare.${module}.git
 	if [ ! -d $gitrepo ]; then
-		git init --bare $gitrepo
-		echo MARK
-		date
+		/usr/local/bin/git init --bare $gitrepo
+		mark
 		if [ $pipeonly -eq 1 ]; then
-			cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo | \
-					git --git-dir $gitrepo fast-import
+			$cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo | \
+					/usr/local/bin/git --git-dir $gitrepo fast-import
 		else
 			ts=`date +%Y%m%d:%H%M%S%z` 
 			dumpfile="dump0.${module}-${ts}" 
-			cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo | \
-					tee $dumpfile | \
-					git --git-dir $gitrepo fast-import
+			$cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo | \
+					/usr/bin/tee $dumpfile | \
+					/usr/local/bin/git --git-dir $gitrepo fast-import
 		fi
-		date
-		echo MARK
+		mark
 		# create non bare git (typical) git repo from bare repo
 		/bin/rm -f ${module}0
-		git clone $gitrepo ${module}0
+		/usr/local/bin/git clone $gitrepo ${module}0
 	else
-		echo MARK
-		date
+		mark
 		if [ $pipeonly -eq 1 ]; then
 			if [ $memcache -eq 1 ]; then
 				# load gitrepo into cache
 				diskgitrepo=$gitrepo
 				memgitrepo=/m/$repodir
 				if [ ! -d $memgitrepo ]; then
-					doas chown $USER /m
-					cp -r ${diskgitrepo}/ /m
-					date
-					echo MARK
+					/usr/bin/doas /sbin/chown $USER /m
+					/bin/cp -r ${diskgitrepo}/ /m
+					mark
 				fi
 				# verify that copy with an sync
-				rsync -a --delete ${diskgitrepo}/ $memgitrepo
-				date
-				echo MARK
+				/usr/local/bin/rsync -a --delete ${diskgitrepo}/ $memgitrepo
+				mark
 
 				# compare in memory cvs and git repo but write to disk git repo
-				cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo $memgitrepo | \
-						git --git-dir $diskgitrepo fast-import
+				$cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo $memgitrepo | \
+						/usr/local/bin/git --git-dir $diskgitrepo fast-import
 
-				date
-				echo MARK
+				mark
 
 				# remove in memory git repo
-				rm -fr $memgitrepo
+				/bin/rm -fr $memgitrepo
 				unset diskgitrepo memgitrepo
 
 			else
-				cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo $gitrepo | \
-						git --git-dir $gitrepo fast-import
+				$cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo $gitrepo | \
+						/usr/local/bin/git --git-dir $gitrepo fast-import
 			fi
 		else
 			ts=`date +%Y%m%d:%H%M%S%z`
 			dumpfile="dump1.${module}-${ts}"
-			cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo $gitrepo | \
-					tee $dumpfile | \
-					git --git-dir $gitrepo fast-import
+			$cvs2gitdump -k OpenBSD -e openbsd.org -m $module $cvsrepo $gitrepo | \
+					/usr/bin/tee $dumpfile | \
+					/usr/local/bin/git --git-dir $gitrepo fast-import
 		fi
-		date
-		echo MARK
+		mark
 		# update non bare git repo (typical) from bare repo
 		if [ ! -d ${module}0 ]; then
-			git clone $gitrepo ${module}0
+			/usr/local/bin/git clone $gitrepo ${module}0
 		fi
-		cd ${module}0 && git pull && cd ..
+		cd ${module}0 && /usr/local/bin/git pull && cd ..
 	fi
 
 done
+
 # remove memcache
 if [ $memcache -eq 1 ]; then
 	if [ -d /m ]; then
-		doas umount -f /m
-		sleep 10
-		doas rmdir /m
+		/usr/bin/doas /sbin/umount -f /m
+		/bin/sleep 10
+		/usr/bin/doas /bin/rmdir /m
 	fi
 fi
+
+mark
 exit 0
